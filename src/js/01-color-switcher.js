5b3726a090be805340e151f686ed1f8e312d6347
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-03-07 21:19:48 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+refs.start.addEventListener('click', addRandomBodyColor);
+
+function addRandomBodyColor() {
+  const color = setInterval(getRandomHexColor(), 1000);
+
+  console.log(color)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
